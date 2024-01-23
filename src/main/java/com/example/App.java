4ad006421f627ccf3cf7b634a7afa62bc8563ff9
aissatou <you@example.com>
2024-01-23 23:35:34 +0000

    first commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,16 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Bonjour tout le monde " );
+        System.out.println( "Ici c'est L3GL" );
+        System.out.println( "Groupe ISI Dakar" );
+    }
+}
